f94e1d15ccfc503025b2b9421bb6b54bbd255d85
Karaskaki <nik.ranchi7@gmail.com>
2024-01-14 03:08:41 +0530

    changes

---
+++ controllers/Auth.js
@@ -0,0 +1,20 @@
+const User = require("../models/User");
+const OTP = require("../models/OTPVerify");
+
+
+// Send OTP
+exports.sendOTP = async (req, res) => {
+
+     // fetch email from request ki body
+     const {email} = req.body;
+
+     // check if user already exist
+     const checkUserPresent = await User.findOne({email});
+
+}
+
+// SignUp
+
+// Login
+
+// Change Password
