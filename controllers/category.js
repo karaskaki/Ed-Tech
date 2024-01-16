5a2e403a49539b1b7230de87fa66a501cf388978
Karaskaki <nik.ranchi7@gmail.com>
2024-01-17 00:34:26 +0530

    add section and subsection

---
+++ controllers/category.js
@@ -0,0 +1,57 @@
+const Tag = require("../models/Category");
+
+exports.createCategory = async (req,res) => {
+
+     try {
+          // Fetch Data
+          const {name, description} = req.body;
+
+          // Validation
+          if(!name || !description) {
+               return res.status(400).json({
+                    success: false,
+                    message: "All fields are required",
+               });
+          }
+
+          // create entry in DB
+          const tagDetails = await  Tag.create({
+               name: name,
+               description: description,
+          });
+          console.log(tagDetails);
+
+          // return response
+          return res.status(200).json({
+               success: true,
+               message: "Tags created successfully",
+          })
+     }
+     catch(error) {
+          return res.status(500).json({
+               success: false,
+               message: error.message,
+          })
+     }
+};
+
+// get All Tags
+exports.showAllCategory = async (req,res) => {
+
+     try {
+          // Find the name and description
+          const allTags = await Tag.find({}, {name: true, description: true});
+          res.status(200).json({
+               success: true,
+               message: "All Tags returned successfully",
+               allTags,
+          })
+
+     }
+     catch(error) {
+          return res.status(500).json({
+               success: false,
+               message: error.message,
+          })
+     }
+}
